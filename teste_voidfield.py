a8e6610ca04ec782803063b27e26539877eefac0
Nicolas Nobrega <nicolas.nobreifrs@gmail.com>
2026-04-01 16:14:04 -0300

    Create teste_voidfield.py

---
+++ teste_voidfield.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager #garante que as versoes sao compativies selenium x chrome
+from selenium.webdriver.common.by import By #comando pronto "por algo"
+import time
+
+
+
